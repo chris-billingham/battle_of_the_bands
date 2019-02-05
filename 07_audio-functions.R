477690280b5e90af2dbca87820dd1c7528f3057b
Chris Billingham <chris.billingham@magairports.com>
2019-02-05 19:37:10 +0000

    just a little bit

--- 07_audio-functions.R
+++ 07_audio-functions.R
@@ -3,7 +3,8 @@
 chunk_and_save <- function(filename) {
   
   r <- readMP3(filename)
-  
+
+# filename stuff ----------------------------------------------------------------
   album <- filename %>%
     str_replace("mp3/", "") %>%
     str_extract("^[a-z0-9_]+")
@@ -16,7 +17,7 @@ chunk_and_save <- function(filename) {
     str_replace_all(" ", "_") %>%
     tolower()
   
-  # convert to mono from stereo, averaging both channels
+  # convert to mono from stereo, averaging both channels ----
   mono_r <- mono(r, "both")
   
   # get the duration for the entire file, we'll need this for later
@@ -40,30 +41,21 @@ chunk_and_save <- function(filename) {
 
 # process a single chunk of music and save off image at a named location
 save_spectrogram <- function(wave_obj, filename) {
-  
-  # extract the left channel
+    # extract the left channel
   r_1s <- wave_obj@left
-  
-  # determine sample rate
+    # determine sample rate
   fs <- wave_obj@samp.rate
-  
-  # determine duration
+    # determine duration
   duration <- length(r_1s)/fs
-  
-  # demean to remove DC offset
+    # demean to remove DC offset
   snd <- r_1s - mean(r_1s)
-  
-  # number of points to use for the fft
+    # number of points to use for the fft
   nfft <- 1024
-  
-  # window size (in points)
+    # window size (in points)
   window <- 128
-  
-  # overlap (in points)
+    # overlap (in points)
   overlap <- 64
-  
-  
-  # create spectrogram
+    # create spectrogram
   spec <- specgram(x = r_1s,
                    n = nfft,
                    Fs = fs,
@@ -73,25 +65,18 @@ save_spectrogram <- function(wave_obj, filename) {
   
   # discard phase information
   P <- abs(spec$S)
-  
   # normalize, but only if we're not in silence
   if(!(mean(P) == 0)) {
     P <- P/max(P)
   }
-  
   # convert to dB, again but not if we're in silence
   if(!(mean(P) == 0)) {
-
   P <- 10*log10(P)
-
   }
-  
   # config time axis
   t <- spec$t
-  
   # plot spectrogram and save
   if(!(mean(P) == 0)) {
-
   png(filename)
   imagep(x = t,
          y = spec$f,
